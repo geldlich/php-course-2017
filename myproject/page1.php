89c3524c8464ed9c86abf95c0bd4a221fbd37991
Veselov <veselov_aradio@bk.ru>
2017-03-11 16:46:23 +0600

    fix all notices

--- myproject/page1.php
+++ myproject/page1.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
 <?php
 $list = [];
 
@@ -21,19 +14,17 @@ while (($buffer = fgets($handle, 4096)) !== false) {
 fclose($handle);
 ?>
 <?php if (count($list) > 0): ?>
-<table style="color: #5892D5">
-  <thead>
-    <tr>
-      <?php foreach ($list as $row): array_map('htmlentities', $row); ?>
-    </tr>
-  </thead>
-  <tbody>
-	<tr>
-      <td><?php echo implode('</td><td>', $row); ?></td>
-    </tr>
-<?php endforeach; ?>
-  </tbody>
-</table>
+  <table style="color: #5892D5">
+    <thead>
+        <?php foreach ($list as $row): array_map('htmlentities', $row); ?>
+    </thead>
+      <tbody>
+        <tr>
+          <td><?php echo implode('</td><td>', $row); ?></td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+  </table>
 <?php endif; ?>
 </body>
 </html>
